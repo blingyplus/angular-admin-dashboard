b7396f88284c22dd9edc9cc40512289aac3c9b9f
Russel D. Boakye <113293972+blingyplus@users.noreply.github.com>
2025-04-14 07:52:59 +0000

    enhance routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,20 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
+import { noAuthGuard } from './guards/no-auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [noAuthGuard],
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    canActivate: [noAuthGuard],
+  },
   {
     path: 'dashboard',
     component: DashboardComponent,
